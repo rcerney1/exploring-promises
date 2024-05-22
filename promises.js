abdec3b20d57f2a18a2cf236c7969462fe48d3a3
Scott Feichter <scottfeichter@gmail.com>
2024-05-22 15:32:12 -0700

    phase 6

--- promises.js
+++ promises.js
@@ -3,47 +3,43 @@
 
 // Your code here
 function num1() {
-    return 1;
-} 
+  return 1;
+}
 
-async function num2(){
-    return 2;
+async function num2() {
+  return 2;
 }
 
 // console.log('num1', num1());
 // console.log('num2', num2());
 // num2().then(res => console.log(res));
 
-
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
-// Your code here 
+// Your code here
 async function waiting() {
-    const value = await num2();
-    console.log('waiting', value);
+  const value = await num2();
+  console.log("waiting", value);
 }
 
 //waiting();
-
-
 
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
 // Your code here
 async function waitForMyPromise() {
-    const promise = new Promise(resolve => {
-        setTimeout(() => {
-            resolve('done!!!!!');
-        }, 2000)
-    });
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("done!!!!!");
+    }, 2000);
+  });
 
-    const result = await promise;
-    console.log('my promise is: ', result);
-} 
+  const result = await promise;
+  console.log("my promise is: ", result);
+}
 //waitForMyPromise();
-
 
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
@@ -55,40 +51,51 @@ async function waitForMyPromise() {
 //     }, 1000)
 // }).then(r => console.log('then my other promise is: ', r));
 
-
-
-
-
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
 // Your code here
 function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-} 
-
-async function test() {
-    await wait(2000);
-    console.log('here is a message')
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-test()
+// async function test() {
+//   await wait(2000);
+//   console.log("here is a message");
+// }
+
+// test();
 
 /* ============================== Phase 6 ============================== */
 /* -------------------- exploring reject and .catch -------------------- */
 
-// Your code here 
+// Your code here
+const tryRandomPromise = (random) =>
+  new Promise((resolve, reject) => {
+    if (random > 0.5) {
+      resolve("success!!!");
+    } else {
+      reject("random error");
+    }
+  });
+
+// console.log(tryRandomPromise(0));
+for (let i = 0; i < 10; i++) {
+  const random = Math.random();
+  wait(2000 + random * 1000)
+    .then(() => tryRandomPromise(random))
+    .then((result) => console.log("random try #", i, random, result))
+    .catch(error => console.error('random try #', i, random, error));
+}
 
 
 
 /* ============================== Phase 7 ============================== */
 /* ---------------- exploring async/await and try/catch ---------------- */
 
-// Your code here 
-
-
+// Your code here
 
 /* ============================== Phase 8 ============================== */
 /* -------------------- Promises are asynchronous! --------------------- */
 
-// Your code here 
+// Your code here
