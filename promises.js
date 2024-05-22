538f1c0341019cfe5929ccdd09360ab4b88d10d4
rcerney <ryanccerney@gmail.com>
2024-05-22 14:59:26 -0700

    phase 5

--- promises.js
+++ promises.js
@@ -1,37 +1,78 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
 
-// Your code here 
+// Your code here
+function num1() {
+    return 1;
+} 
 
+async function num2(){
+    return 2;
+}
+
+// console.log('num1', num1());
+// console.log('num2', num2());
+// num2().then(res => console.log(res));
 
 
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
 // Your code here 
+async function waiting() {
+    const value = await num2();
+    console.log('waiting', value);
+}
+
+//waiting();
 
 
 
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
-// Your code here 
+// Your code here
+async function waitForMyPromise() {
+    const promise = new Promise(resolve => {
+        setTimeout(() => {
+            resolve('done!!!!!');
+        }, 2000)
+    });
 
+    const result = await promise;
+    console.log('my promise is: ', result);
+} 
+//waitForMyPromise();
 
 
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
 
-// Your code here 
+// Your code here
+// new Promise((resolve) => {
+//     setTimeout(()=> {
+//         resolve('done!!');
+//     }, 1000)
+// }).then(r => console.log('then my other promise is: ', r));
+
+
 
 
 
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
-// Your code here 
+// Your code here
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+} 
 
+async function test() {
+    await wait(2000);
+    console.log('here is a message')
+}
 
+test()
 
 /* ============================== Phase 6 ============================== */
 /* -------------------- exploring reject and .catch -------------------- */
